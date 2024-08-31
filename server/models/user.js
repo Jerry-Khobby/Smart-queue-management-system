bd05590e55f3aa910976ef92e5f32860b039b527
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-08-31 02:01:32 -1200

    removed .env file

--- server/models/user.js
+++ server/models/user.js
@@ -4,9 +4,19 @@ const mongoose = require("mongoose");
 
 const Schema =mongoose.Schema;
 const User = new Schema({
+  userId:{
+type:monogoose.Schema.Types.ObjectId,
+auto:true,
+unique:true,
+},
   name:{
     type:String,
     required:true,
+  },
+  role: {
+    type: String,
+    enum: ['receptionist', 'nurse', 'doctor', 'admin'],
+    required: true,
   },
   email:{
     type:String,
@@ -20,7 +30,26 @@ const User = new Schema({
   dateEntered:{
     type:Date,
     default:Date.now(),
-  }
+  },
+  contactNumber: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true, // e.g., 'Emergency', 'Pediatrics', 'General Medicine'
+  },  
+  shiftStartTime: {
+    type: Date,
+    required: true,
+  },
+  shiftEndTime: {
+    type: Date,
+    required: true,
+  },
+  profileImage: {
+    type: String, // URL to the health worker's profile image
+  },
 
 })
 
